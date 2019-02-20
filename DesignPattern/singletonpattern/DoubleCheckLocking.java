3b83a6f01f057f7243b3057edf96fa299a912eea
quangnt <quang217@gmail.com>
2019-02-20 13:31:53 +0700

    commit DoubleCheckLocking (Singleton Pattern)

---
+++ DesignPattern/singletonpattern/DoubleCheckLocking.java
@@ -0,0 +1,23 @@
+package singletonpattern;
+
+public class DoubleCheckLocking {
+	private static volatile DoubleCheckLocking instance;
+	 
+    private DoubleCheckLocking() {
+    }
+    public static DoubleCheckLocking getInstance() {
+        // Do something before get instance ...
+        if (instance == null) {
+            // Do the task too long before create instance ...
+            // Block so other threads cannot come into while initialize
+            synchronized (DoubleCheckLocking.class) {
+                // Re-check again. Maybe another thread has initialized before
+                if (instance == null) {
+                    instance = new DoubleCheckLocking();
+                }
+            }
+        }
+        // Do something after get instance ...
+        return instance;
+    }
+}
